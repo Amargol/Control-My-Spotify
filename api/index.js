111874eb7d69b51af3c63c157fa71219169eacf1
Shreyas Amargol <shreyasamargol@Shreyass-MBP-2.attlocal.net>
2020-05-02 16:30:34 -0500

    fixed scopes

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ app.get('/callback', (req, res) => {
 
 // Use this to get code
 // app.get('/login', function(req, res) {
-//     var scopes = 'user-modify-playback-state';
+//     var scopes = 'user-modify-playback-state user-read-recently-played';
 //     res.redirect('https://accounts.spotify.com/authorize' +
 //       '?response_type=code' +
 //       `&client_id=${process.env.SPOTIFY_CLIENT_ID}`+
@@ -34,7 +34,7 @@ app.get('/callback', (req, res) => {
 //       `&redirect_uri=${process.env.CLIENT_URL}/api/callback`)
 // })
 
-// Use code to get token
+// // Use code to get token
 // app.get('/refresh-token', (req, res) => {
 //     var headers = {
 //         'Accept': 'application/json',
@@ -47,7 +47,7 @@ app.get('/callback', (req, res) => {
 //         headers: headers,
 //         qs: {
 //             grant_type: "authorization_code",
-//             code: "AQCOT0oUUbf2TI3kMf-K961C1TdFqPTyLMMqST-VVVi-q5JBb548W2IvACe2zYc1E72EdR6uQOGfwhnBh15xN6ndBqE_Z3UIVwYfRGj-_aUHmaTVjnvLWphng4rQORxcxPuluk5ZOJiN62xxEEvYCF-YEFA8-xeH8je8GdeKIUEXV7Gg5SApFbag8HoPYPxhCt1OBCzBtsErkJc3edKSzalowhoPd03ofPuk9GVs-0IFwbT9",
+//             code: "AQCCwCk3pQUz4pWbZe-U5dZYdWPLlAVfz20g_6MEDIHMFfbxQAuQzReNNXNnU1LzRdPmxalbfld8g1xc_Qul5uvXyaE-vafAWtvTNZSCrOZcmhnEb2mX9-gEiI-N0ocRyDdkj7m2RmnZsSY5aCYlboRfa83Y1BquxqsxzB_be9itwbDUO71aFYu7iTh49g9o_jlwiCx06H5eXIdMJIeTBcYvo9KSjFEWLwrEPyXjd4FZzoQpDmnkzJKfinv9vuNCHbjrSU6jaRSkOTz_u9Q0",
 //             redirect_uri: `${process.env.CLIENT_URL}/api/callback`,   
 //             client_id: process.env.SPOTIFY_CLIENT_ID,
 //             client_secret: process.env.SPOTIFY_CLIENT_SECRET
@@ -148,30 +148,70 @@ app.get('/skip', async (req, res) => {
     })
 })
 
+app.get('/recently-played', async (req, res) => {
+    let at = await getAccessToken()
 
-app.get('/hi', (req, res) => {
-    var headers = {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer BQChOdIkv1lg9SXg15PBfSJuM-IVWWFZ1Eu8_sVMcwImUt9nR6x4Atec-zONLFH4iSpb3docJRgXsrOzyvI87KhCAsdA_IgUOxhDr2nfEiDu06F2tHUUFFiarAKVD2ZP01p0fPSwONIJTRal5nWIZNPBpqmdefc04kZBz1usunyPpYfnsgvatuA'
-    };
-    
-    var options = {
-        url: 'https://api.spotify.com/v1/me/player/currently-playing',
-        headers: headers
-    };
-    
-    function callback(error, response, body) {
-        if (!error && response.statusCode == 200) {
-            res.send(body);
+    var {status, statusText, data} = await axios({
+        method: 'get',
+        url: 'https://api.spotify.com/v1/me/player/recently-played',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${at}`
+        },
+        params: {
+            'limit': 10
         }
+    }).catch((error) => {
+        console.log(error)
+        return {
+            status: 400,
+            statusText: "Bad Request",
+            data: ""
+        }
+    })
+
+    data = data.items.map(e => (
+        {
+            "name": e.track.name,
+            "artist": e.track.artists.map(a => a.name).join(", "),
+            "image": e.track.album.images[e.track.album.images.length - 1].url,
+            "href": e.track.href
+        }
+    ))
+
+    res.send({
+        status,
+        statusText,
+        data
+    })
+})
+
+
+app.get('/hi', async (req, res) => {
+    let at = await getAccessToken()
+
+    for (let i = 0; i < 4; i++) {
+        var {status, statusText, data} = await axios({
+            method: 'post',
+            url: 'https://api.spotify.com/v1/me/player/next',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${at}`
+            }
+        }).catch((error) => {
+            console.log(error)
+            return {
+                status: 400,
+                statusText: "Bad Request",
+                data: ""
+            }
+        })
     }
-    
-    request(options, callback);
 })
 
 module.exports = {
     path: '/api/',
     handler: app
 }
-  
